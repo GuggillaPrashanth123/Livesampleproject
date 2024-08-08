55b46367c6cadda3b4020b1f0d1b73dfadfd9df1
deepthi <deepthi@LAPTOP-A0510HNN>
2024-08-08 16:04:19 +0530

    new code added

---
+++ Sample_project/src/aug8/Codes.java
@@ -0,0 +1,13 @@
+package aug8;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Codes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver = new ChromeDriver();
+	}
+
+}
